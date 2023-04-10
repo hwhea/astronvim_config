7373d6ea198016a97608741431225f309307f9bb
Harrie <harriegwheatley@gmail.com>
2023-04-10 23:20:57 +0100

    add readme

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,4 +3,13 @@ return {
   opts = {
     ensure_installed = { "lua" },
   },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = function(_, opts)
+      require("nvim-treesitter/nvim-treesitter-context").setup {
+        enabled = true,
+      }
+    end,
+  },
 }
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Harrie's Config
+
+## Installation
+1. Install ripgrep `brew install ripgrep`
+2. Back up the default config.
+3. Clone this config to ~/.config/nvim/lua/user 
